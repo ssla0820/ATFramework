0ffb9539d4580924bdad5e00c22f45e9ae8500f7
ssla0820 <debby870820@gmail.com>
2024-12-06 11:13:42 +0800

    Add Framework1Page and Framework2Page classes with basic functionality

---
+++ Framework1/Framework1.py
@@ -0,0 +1,5 @@
+class Framework1Page():
+    def __init__(self, name):
+        self.name = name
+    def data(self):
+        print(f'calling Framework1.data() from {self.name}')
---
+++ Framework2/Framework2.py
@@ -0,0 +1,5 @@
+class Framework2Page():
+    def __init__(self, name):
+        self.name = name
+    def data(self):
+        print(f'calling Framework2.data() from {self.name}')
